877133fd25e203dad8736bf2c1060d57abd24f03
Linus Kirkwood <linuskirkwood@gmail.com>
2023-06-07 16:17:06 +1000

    Replaced read-write with just write where possible

--- functions.lua
+++ functions.lua
@@ -14,25 +14,23 @@ local function create_dns(_, args)
   elseif plugin == nil then return 'Must provide plugin name'
   end
 
-  if redis.call('SISMEMBER', DNS_KEY, name) == 0 then
+  if redis.call('SADD', DNS_KEY, name) ~= 0 then
     create_change('create dns name', name, plugin)
-    redis.call('SADD', DNS_KEY, name)
   end
 
   redis.call('SADD', string.format('%s;%s;plugins', DNS_KEY, name), plugin)
 
   local value_set = string.format('%s;%s;%s;%s', DNS_KEY, name, plugin, rtype)
-  if redis.call('SISMEMBER', value_set, value) == 0 then
+  if redis.call('SADD', value_set, value) ~= 0 then
     create_change(
       'create dns record',
       string.format('%s --(%s)-> %s', name, rtype, value),
       plugin
     )
-    redis.call('SADD', value_set, value)
   end
 end
 
-local NODE_KEY = 'nodes'
+local NODES_KEY = 'nodes'
 
 local function create_node(_, args)
   local name = table.remove(args, 1)
@@ -41,29 +39,29 @@ local function create_node(_, args)
   local dns_names = args
   table.sort(dns_names)
 
-  local node = string.format('%s;%s', NODE_KEY, table.concat(dns_names, ';'))
+  local node_id = table.concat(dns_names, ';')
+  local node_key = string.format('%s;%s', NODES_KEY, node_id)
 
   -- Record node exists with these dns names.
-  if redis.call('SISMEMBER', NODE_KEY, node) == 0 then
+  if redis.call('SADD', NODES_KEY, node_id) ~= 0 then
     create_change(
       'create node with names',
       table.concat(dns_names, ', '),
       plugin
     )
-    redis.call('SADD', NODE_KEY, node)
   end
 
   -- Add plugin to list of plugins providing a node with these dns names.
-  redis.call('SADD', string.format('%s;plugins', node), plugin)
+  redis.call('SADD', string.format('%s;plugins', node_key), plugin)
 
-  local node_plugin_details = string.format('%s;%s', node, plugin)
+  local node_plugin_details = string.format('%s;%s', node_key, plugin)
 
   -- Record changes to plugin version of node details
   local old_name = redis.call('HGET', node_plugin_details, 'name')
   if old_name ~= name then
     create_change(
       'plugin updated node name',
-      string.format('(%s) %s ---> %s', node, old_name, name),
+      string.format('(%s) %s ---> %s', node_id, old_name, name),
       plugin
     )
   end
@@ -72,7 +70,7 @@ local function create_node(_, args)
   if old_exc ~= exclusive then
     create_change(
       'plugin updated node exclusivity',
-      string.format('(%s) %s ---> %s', node, old_exc, exclusive),
+      string.format('(%s) %s ---> %s', node_key, old_exc, exclusive),
       plugin
     )
   end
@@ -82,7 +80,7 @@ local function create_node(_, args)
     'name', name, 'exclusive', exclusive
   )
 
-  return node
+  return node_key
 end
 
 redis.register_function('netdox_create_change', create_change)
