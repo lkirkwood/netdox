2687258f9621a324610c16e2631c951d6afb0817
Linus Kirkwood <linuskirkwood@gmail.com>
2023-06-26 17:31:24 +1000

    Plugins receive config fields as toml str on stdin

--- netdox/src/plugins.rs
+++ netdox/src/plugins.rs
@@ -2,7 +2,6 @@ use std::{collections::HashMap, process::Command};
 
 use crate::{
     config::PluginConfig,
-    config_err,
     error::{NetdoxError, NetdoxResult},
     plugin_err,
 };
@@ -16,30 +15,49 @@ pub struct PluginResult {
 /// Runs all plugins and returns their result.
 /// TODO add passing plugin config to plugin binary.
 pub fn update(plugins: Vec<PluginConfig>) -> NetdoxResult<Vec<PluginResult>> {
-    let mut children = HashMap::new();
-    let mut config = HashMap::new();
+    let mut cmds = HashMap::new();
     for plugin in plugins {
-        if children.contains_key(&plugin.name) {
-            return config_err!(format!(
+        if cmds.contains_key(&plugin.name) {
+            return plugin_err!(format!(
                 "Plugin name {} appears multiple times.",
                 plugin.name
             ));
         }
-        let proc = match Command::new(&plugin.path).spawn() {
-            Err(err) => {
-                return plugin_err!(format!(
-                    "Failed to spawn subprocess for plugin {}: {err}",
-                    plugin.name
-                ))
+
+        let mut cmd = Command::new(&plugin.path);
+
+        let field_str = toml::to_string(&plugin.fields);
+        if let Err(err) = field_str {
+            return plugin_err!(format!(
+                "Failed to serialize additional config fields for {}: {err}",
+                plugin.name
+            ));
+        }
+
+        cmd.arg(field_str.unwrap());
+        cmds.insert(plugin.name.clone(), cmd);
+    }
+
+    let mut procs = HashMap::new();
+    for (name, mut cmd) in cmds {
+        match cmd.spawn() {
+            Ok(proc) => {
+                procs.insert(name, proc);
             }
-            Ok(_p) => _p,
-        };
-        children.insert(plugin.name.clone(), proc);
-        config.insert(plugin.name.clone(), plugin);
+            Err(err) => {
+                println!("Killing all existing subprocesses due to error spawning new one...");
+                for mut proc in procs {
+                    if let Err(err) = proc.1.kill() {
+                        println!("Failed to kill process for plugin {}: {err}", proc.0);
+                    }
+                }
+                return plugin_err!(format!("Failed to spawn process for plugin {name}: {err}"));
+            }
+        }
     }
 
     let mut results = vec![];
-    for (name, proc) in children {
+    for (name, proc) in procs {
         let output = match proc.wait_with_output() {
             Err(err) => {
                 return plugin_err!(format!(
