b79ae11893213f940eaa294479d465c960d1fd1e
Linus Kirkwood <linuskirkwood@gmail.com>
2023-07-26 16:52:25 +1000

    Added process tests.

--- src/process/tests.rs
+++ src/process/tests.rs
@@ -10,8 +10,11 @@ use crate::{
 
 use super::model::ResolvedNode;
 
-fn mock_node() -> ResolvedNode {
-    ResolvedNode {
+#[test]
+fn test_process_1() {
+    let mut client = setup_db();
+    let mut con = client.get_connection().unwrap();
+    let mock = ResolvedNode {
         name: "linkable-name".to_string(),
         link_id: "!link_id!".to_string(),
         alt_names: HashSet::from(["soft-name".to_string()]),
@@ -21,13 +24,7 @@ fn mock_node() -> ResolvedNode {
             "[private-net]192.168.0.1".to_string(),
         ]),
         plugins: HashSet::from([PLUGIN.to_string()]),
-    }
-}
-
-#[test]
-fn test_process() {
-    let mut client = setup_db();
-    let mut con = client.get_connection().unwrap();
+    };
 
     // Setup dns records for merging.
     call_fn(
@@ -49,13 +46,18 @@ fn test_process() {
         ],
     );
 
-    let link_id = "!link_id!";
-
     // Create linkable node.
     call_fn(
         &mut con,
         "netdox_create_node",
-        &["1", "domain.net", PLUGIN, "linkable-name", "false", link_id],
+        &[
+            "1",
+            "domain.net",
+            PLUGIN,
+            &mock.name,
+            "false",
+            &mock.link_id,
+        ],
     );
 
     process(&mut client).unwrap();
@@ -65,6 +67,66 @@ fn test_process() {
         .query::<String>(&mut con)
         .expect(&format!("Failed to select db {PROC_DB}"));
 
-    let node = ResolvedNode::read(&format!("{NODES_KEY};{link_id}"), &mut con).unwrap();
-    assert_eq!(node, mock_node());
+    let node = ResolvedNode::read(&format!("{NODES_KEY};{}", mock.link_id), &mut con).unwrap();
+    assert_eq!(mock, node);
+}
+
+#[test]
+fn test_process_2() {
+    let mut client = setup_db();
+    let mut con = client.get_connection().unwrap();
+    let mock = ResolvedNode {
+        name: "linkable-node".to_string(),
+        link_id: "!link_id!".to_string(),
+        alt_names: HashSet::from(["soft-matches".to_string()]),
+        dns_names: HashSet::from([
+            "[default-net]domain.com".to_string(),
+            "[private-net]0.0.0.0".to_string(),
+        ]),
+        plugins: HashSet::from([PLUGIN.to_string()]),
+    };
+
+    // Create soft nodes
+    call_fn(
+        &mut con,
+        "netdox_create_node",
+        &["1", "domain.com", PLUGIN, "soft-matches"],
+    );
+    call_fn(
+        &mut con,
+        "netdox_create_node",
+        &["2", "domain.net", "domain.com", PLUGIN, "soft-nomatch"],
+    );
+
+    // Link soft nodes (should merge if linkable node not exclusive, as tested above.)
+    call_fn(
+        &mut con,
+        "netdox_create_dns",
+        &["1", "domain.net", PLUGIN, "cname", "domain.com"],
+    );
+
+    // Create linkable, exclusive node.
+    call_fn(
+        &mut con,
+        "netdox_create_node",
+        &[
+            "2",
+            "domain.com",
+            "[private-net]0.0.0.0",
+            PLUGIN,
+            "linkable-node",
+            "true",
+            "!link_id!",
+        ],
+    );
+
+    process(&mut client).unwrap();
+
+    redis::cmd("SELECT")
+        .arg(PROC_DB)
+        .query::<String>(&mut con)
+        .expect(&format!("Failed to select db {PROC_DB}"));
+
+    let node = ResolvedNode::read(&format!("{NODES_KEY};{}", mock.link_id), &mut con).unwrap();
+    assert_eq!(mock, node);
 }
