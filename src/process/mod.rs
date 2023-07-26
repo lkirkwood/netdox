a32b0b5ec5a344bd9e92bf37e9ae0c165f3af0fc
Linus Kirkwood <linuskirkwood@gmail.com>
2023-07-26 16:52:08 +1000

    Updated node resolution to handle exclusive nodes.

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -4,12 +4,12 @@ mod tests;
 
 use std::collections::{hash_map::Entry, HashMap, HashSet};
 
-use paris::{error, warn};
+use paris::warn;
 use redis::{Client, Commands, Connection};
 
 use crate::{
     error::{NetdoxError, NetdoxResult},
-    redis_err,
+    process_err, redis_err,
 };
 use model::*;
 
@@ -221,46 +221,112 @@ fn map_nodes<'a>(
 
 // RESOLVED NODES
 
+// TODO refactor these two fns with better names.
+fn _resolve_nodes(nodes: Vec<&RawNode>) -> NetdoxResult<Option<ResolvedNode>> {
+    let num_nodes = nodes.len();
+    let mut linkable = None;
+    let mut alt_names = HashSet::new();
+    let mut dns_names = HashSet::new();
+    let mut plugins = HashSet::new();
+    for node in &nodes {
+        plugins.insert(node.plugin.clone());
+        dns_names.extend(node.dns_names.clone());
+        if node.link_id.is_some() {
+            if linkable.is_none() {
+                linkable = Some(node);
+            } else {
+                // TODO review this behaviour
+                return process_err!(format!(
+                    "Nodes in set {nodes:?} have multiple link ids: {}, {}",
+                    linkable.as_ref().unwrap().link_id.as_ref().unwrap(),
+                    node.link_id.as_ref().unwrap()
+                ));
+            }
+        } else {
+            alt_names.insert(node.name.clone());
+        }
+    }
+
+    if let Some(node) = linkable {
+        Ok(Some(ResolvedNode {
+            name: node.name.clone(),
+            alt_names,
+            dns_names,
+            link_id: node.link_id.clone().unwrap(),
+            plugins,
+        }))
+    } else if num_nodes > 1 {
+        process_err!(format!(
+            "Found matching soft nodes with no link id: {nodes:?}"
+        ))
+    } else {
+        Ok(None)
+    }
+}
+
+// TODO handle nodes from same plugin with same dns names.
 /// Consolidates raw nodes into resolved nodes.
 fn resolve_nodes(dns: &DNS, nodes: Vec<RawNode>) -> NetdoxResult<Vec<ResolvedNode>> {
     let mut resolved = Vec::new();
-    for (superset, nodes) in map_nodes(dns, nodes.iter().collect())? {
-        let num_nodes = nodes.len();
-        let mut linkable = None;
-        let mut alt_names = HashSet::new();
-        let mut dns_names = HashSet::new();
-        let mut plugins = HashSet::new();
-        for node in nodes {
-            plugins.insert(node.plugin.clone());
-            dns_names.extend(node.dns_names.clone());
-            if node.link_id.is_some() {
-                if linkable.is_none() {
-                    linkable = Some(node);
-                } else {
-                    // TODO review this behaviour
-                    error!(
-                        "Nodes under superset {superset:?} have multiple link ids: {}, {}",
-                        linkable.as_ref().unwrap().link_id.as_ref().unwrap(),
-                        node.link_id.as_ref().unwrap()
-                    );
-                    break;
+
+    // Splits nodes into permissive (not exclusive) and exclusive.
+    let (mut exclusive, permissive): (Vec<_>, Vec<_>) = nodes.iter().partition(|n| n.exclusive);
+    exclusive.sort_by(|n1, n2| n1.dns_names.len().cmp(&n2.dns_names.len()));
+
+    // Splits nodes into exclusive + permissive matches, and unmatched nodes.
+    let (mut exc_matches, mut unmatched): (HashMap<&RawNode, Vec<&RawNode>>, Vec<&RawNode>) =
+        (HashMap::new(), vec![]);
+
+    for perm_node in permissive {
+        let mut exc_match = false;
+
+        // Check if permissive node matches any exclusive nodes.
+        for exc_node in &exclusive {
+            if perm_node.dns_names.is_subset(&exc_node.dns_names) {
+                match exc_matches.entry(exc_node) {
+                    Entry::Vacant(entry) => {
+                        entry.insert(vec![perm_node]);
+                    }
+                    Entry::Occupied(mut entry) => {
+                        entry.get_mut().push(perm_node);
+                    }
                 }
-            } else {
-                alt_names.insert(node.name.clone());
+                exc_match = true;
+                break;
             }
         }
 
-        if let Some(node) = linkable {
-            resolved.push(ResolvedNode {
-                name: node.name.clone(),
-                alt_names,
-                dns_names,
-                link_id: node.link_id.clone().unwrap(),
-                plugins,
-            });
-        } else if num_nodes > 1 {
-            warn!("Matching soft nodes with no link id under superset {superset:?}");
+        if !exc_match {
+            unmatched.push(perm_node);
         }
+    }
+
+    // Add unmatched exclusive nodes to unmatched node pool.
+    for exc_node in exclusive {
+        if !exc_matches.contains_key(exc_node) {
+            unmatched.push(exc_node);
+        }
+    }
+
+    // Resolve exclusive node match groups.
+    for (exc_node, mut matching_nodes) in exc_matches {
+        matching_nodes.push(exc_node);
+        match _resolve_nodes(matching_nodes)? {
+            Some(node) => resolved.push(node),
+            None => warn!(
+                "Failed to create resolved node from set of nodes matching exclusive node: {exc_node:?}"
+            )
+        }
+    }
+
+    // Resolve match groups for all unmatched nodes.
+    for (superset, nodes) in map_nodes(dns, unmatched)? {
+        match _resolve_nodes(nodes)? {
+            Some(node) => resolved.push(node),
+            None => warn!(
+                "Failed to create resolved node from set of nodes under superset: {superset:?}"
+            ),
+        };
     }
 
     Ok(resolved)
