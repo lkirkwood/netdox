924caa78d011c6d89643b315cf28ed54e4f916bc
Linus Kirkwood <linuskirkwood@gmail.com>
2023-08-24 15:04:33 +1000

    Added remote config

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,3 +2,4 @@ pub mod local;
 pub mod remote;
 
 pub use local::{LocalConfig, PluginConfig};
+pub use remote::RemoteConfig;
--- src/config/remote.rs
+++ src/config/remote.rs
@@ -1,1 +1,11 @@
+use std::collections::{HashMap, HashSet};
 
+#[derive(PartialEq, Eq, Debug)]
+pub struct RemoteConfig {
+    /// A set of DNS names to exclude from all networks.
+    pub exclude_dns: HashSet<String>,
+    /// Maps network-qualified subnets to locations.
+    pub locations: HashMap<String, String>, // TODO proper subnet type
+    /// A map of plugin config values for each dns/node object.
+    pub plugin_cfg: HashMap<String, HashMap<String, String>>,
+}
