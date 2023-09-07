8ba8d5117685689164373950604815d431130cfc
Linus Kirkwood <linuskirkwood@gmail.com>
2023-09-07 16:55:14 +1000

    Updated pageseeder remote

--- src/remote/pageseeder.rs
+++ src/remote/pageseeder.rs
@@ -1,5 +1,8 @@
 use crate::config::RemoteConfig;
-use crate::error::NetdoxResult;
+use crate::error::{NetdoxError, NetdoxResult};
+use async_trait::async_trait;
+use pageseeder::api::{oauth::PSCredentials, PSServer};
+use pageseeder::error::PSError;
 use serde::{Deserialize, Serialize};
 
 const REMOTE_CONFIG_PATH: &str = "website/config.psml";
@@ -13,12 +16,32 @@ pub struct PSRemote {
     group: String,
 }
 
+impl From<PSError> for NetdoxError {
+    fn from(value: PSError) -> Self {
+        Self::Remote(value.to_string())
+    }
+}
+
+#[async_trait]
 impl crate::remote::RemoteInterface for PSRemote {
-    fn test(&self) -> NetdoxResult<()> {
+    async fn test(&self) -> NetdoxResult<()> {
         Ok(())
     }
 
-    fn config(&self) -> NetdoxResult<RemoteConfig> {
-        todo!("Implement pulling config from PS.")
+    async fn config(&self) -> NetdoxResult<RemoteConfig> {
+        let mut server = PSServer::new(
+            self.url.clone(),
+            PSCredentials::ClientCredentials {
+                id: self.client_id.clone(),
+                secret: self.client_secret.clone(),
+            },
+        );
+        let thread = server
+            .uri_export(&self.username, REMOTE_CONFIG_PATH)
+            .await?;
+
+        println!("{thread:?}");
+
+        todo!("Download files from thread")
     }
 }
