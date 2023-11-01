c7e36357abb5d636006e9f6a5d42858e0fa2be61
Linus Kirkwood <linuskirkwood@gmail.com>
2023-11-01 14:56:24 +1100

    Added creating dns record publish for ps

--- src/remote/pageseeder/publish.rs
+++ src/remote/pageseeder/publish.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use crate::{
     data::{
-        model::{Change, ChangeType, DNS_KEY, NODES_KEY},
+        model::{Change, ChangeType, DNSRecord, DNS_KEY, NODES_KEY},
         DataClient, DataConn,
     },
     error::{NetdoxError, NetdoxResult},
@@ -15,18 +15,30 @@ use super::{
     PSRemote,
 };
 use async_trait::async_trait;
+use pageseeder::psml::model::{Fragments, PropertiesFragment};
+use quick_xml::se as xml_se;
 
 #[async_trait]
 pub trait PSPublisher {
-    /// Updates the fragment with the metadata at key.
+    /// Adds a DNS record to relevant document given the changelog change value.
+    /// Also adds an implied DNS record to the destination document if there is no equivalent record already,
+    /// implied or otherwise.
+    async fn add_dns_record(
+        &self,
+        mut backend: Box<dyn DataConn>,
+        value: String,
+    ) -> NetdoxResult<()>;
+
+    /// Updates the fragment with the metadata change from the change value.
     async fn update_metadata(
         &self,
         mut backend: Box<dyn DataConn>,
-        key: String,
+        value: String,
     ) -> NetdoxResult<()>;
 
-    /// Updates the fragment with the plugin data at key.
-    async fn update_pdata(&self, mut backend: Box<dyn DataConn>, key: String) -> NetdoxResult<()>;
+    /// Updates the fragment with the plugin data change from the change value.
+    async fn update_pdata(&self, mut backend: Box<dyn DataConn>, value: String)
+        -> NetdoxResult<()>;
 
     /// Applies a series of changes to the PageSeeder documents on the remote.
     /// Will attempt to update in place where possible.
@@ -39,29 +51,104 @@ pub trait PSPublisher {
 
 #[async_trait]
 impl PSPublisher for PSRemote {
+    async fn add_dns_record(&self, _backend: Box<dyn DataConn>, value: String) -> NetdoxResult<()> {
+        let mut val_iter = value.split(';').into_iter().skip(1);
+        let name = match val_iter.next() {
+            Some(name) => name.to_string(),
+            None => {
+                return redis_err!(format!(
+                    "Invalid created dns record change value (missing qname): {value}"
+                ))
+            }
+        };
+
+        let plugin = match val_iter.next() {
+            Some(plugin) => plugin.to_string(),
+            None => {
+                return redis_err!(format!(
+                    "Invalid created dns record change value (missing plugin): {value}"
+                ))
+            }
+        };
+
+        let rtype = match val_iter.next() {
+            Some(rtype) => rtype.to_string(),
+            None => {
+                return redis_err!(format!(
+                    "Invalid created dns record change value (missing rtype): {value}"
+                ))
+            }
+        };
+
+        let value = match val_iter.next() {
+            Some(value) => value.to_string(),
+            None => {
+                return redis_err!(format!(
+                    "Invalid created dns record change value (missing record value): {value}"
+                ))
+            }
+        };
+
+        let docid = dns_qname_to_docid(&name);
+        let record = DNSRecord {
+            name,
+            value,
+            rtype,
+            plugin,
+        };
+
+        let fragment = PropertiesFragment::from(&record);
+        match xml_se::to_string(&fragment) {
+            Ok(content) => {
+                self.server()
+                    .put_uri_fragment(
+                        &self.username,
+                        &self.group,
+                        &docid,
+                        &fragment.id,
+                        content,
+                        None,
+                    )
+                    .await?;
+            }
+            Err(err) => {
+                return io_err!(format!(
+                    "Failed to serialise DNS record to PSML: {}",
+                    err.to_string()
+                ))
+            }
+        }
+
+        Ok(())
+    }
+
     async fn update_metadata(
         &self,
         mut backend: Box<dyn DataConn>,
-        key: String,
+        value: String,
     ) -> NetdoxResult<()> {
-        let mut key_iter = key.split(';').into_iter().skip(1);
-        let (metadata, docid) = match key_iter.next() {
+        let mut val_iter = value.split(';').into_iter().skip(1);
+        let (metadata, docid) = match val_iter.next() {
             Some(NODES_KEY) => {
                 let node = backend
-                    .get_node(&key_iter.collect::<Vec<&str>>().join(";"))
+                    .get_node(&val_iter.collect::<Vec<&str>>().join(";"))
                     .await?;
                 let metadata = backend.get_node_metadata(&node).await?;
                 (metadata, node_id_to_docid(&node.link_id))
             }
             Some(DNS_KEY) => {
-                let qname = &key_iter.collect::<Vec<&str>>().join(";");
+                let qname = &val_iter.collect::<Vec<&str>>().join(";");
                 let metadata = backend.get_dns_metadata(qname).await?;
                 (metadata, dns_qname_to_docid(qname))
             }
-            _ => return redis_err!(format!("Invalid updated metadata change key: {key}")),
+            _ => {
+                return redis_err!(format!(
+                    "Invalid updated metadata change value (wrong first segment): {value}"
+                ))
+            }
         };
 
-        match quick_xml::se::to_string(&metadata_fragment(metadata)) {
+        match xml_se::to_string(&metadata_fragment(metadata)) {
             Ok(content) => {
                 self.server()
                     .put_uri_fragment(
@@ -101,10 +188,30 @@ impl PSPublisher for PSRemote {
                 }
             }
             Some(DNS_KEY) => key_iter.collect::<Vec<&str>>().join(";"),
-            _ => return redis_err!(format!("Invalid updated plugin data change key: {key}")),
+            _ => return redis_err!(format!("Invalid updated plugin data change value: {key}")),
         };
 
-        todo!("Update the document on PS");
+        let fragment = Fragments::from(pdata);
+        let id = match &fragment {
+            Fragments::Fragment(frag) => &frag.id,
+            Fragments::Media(_frag) => todo!("Media fragment in pageseeder-rs"),
+            Fragments::Properties(frag) => &frag.id,
+            Fragments::Xref(frag) => &frag.id,
+        };
+
+        match xml_se::to_string(&fragment) {
+            Ok(content) => {
+                self.server()
+                    .put_uri_fragment(&self.username, &self.group, &docid, id, content, None)
+                    .await?;
+            }
+            Err(err) => {
+                return io_err!(format!(
+                    "Failed to serialise plugin data to PSML: {}",
+                    err.to_string()
+                ))
+            }
+        }
 
         Ok(())
     }
@@ -139,14 +246,11 @@ impl PSPublisher for PSRemote {
                 CT::UpdatedMetadata => {
                     updates.push(self.update_metadata(client.get_con().await?, change.value));
                 }
-                CT::UpdatedPluginDataList
-                | CT::UpdatedPluginDataMap
-                | CT::UpdatedPluginDataString => {
-                    todo!("Update plugin data")
+                CT::UpdatedPluginData => {
+                    updates.push(self.update_pdata(client.get_con().await?, change.value));
                 }
                 CT::AddPluginToDnsName => todo!("Add plugin to dns name"),
-                CT::AddRecordTypeToDnsName => todo!("Add record type to dns name"),
-                CT::CreateDnsRecord => todo!("Create dns record"),
+                CT::CreateDnsRecord => {}
                 CT::UpdatedNetworkMapping => todo!("Update network mappings"),
             }
         }
