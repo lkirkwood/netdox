7a5d7fcf8187e22d302404f61016b915100cdb87
Linus Kirkwood <linuskirkwood@gmail.com>
2024-01-31 19:20:59 +1100

    Removed debug function

--- functions.lua
+++ functions.lua
@@ -309,31 +309,6 @@ local function create_data_str(data_key, plugin, title, content_type, content)
     end
 end
 
-local function table_to_string(tbl)
-    local result = "{"
-    for k, v in pairs(tbl) do
-        -- Check the key type (ignore any numerical keys - assume its an array)
-        if type(k) == "string" then
-            result = result .. '["' .. k .. '"]' .. "="
-        end
-
-        -- Check the value type
-        if type(v) == "table" then
-            result = result .. table_to_string(v)
-        elseif type(v) == "boolean" then
-            result = result .. tostring(v)
-        else
-            result = result .. '"' .. v .. '"'
-        end
-        result = result .. ","
-    end
-    -- Remove leading commas from the result
-    if result ~= "" then
-        result = result:sub(1, result:len() - 1)
-    end
-    return result .. "}"
-end
-
 local function create_data_hash(data_key, plugin, title, content)
     local created = false
     local changed = false
