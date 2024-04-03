lkirkwood/netdox

commit 99b7feee19c64a8d94afa9c88ac0a60c3edb5d97

chore: Moved NETDOX_PLUGIN constant to data::model

src/process.rs
@@ -6,13 +6,10 @@ use std::collections::{hash_map::Entry, HashMap, HashSet};
 use paris::warn;
 
 use crate::{
-    data::model::*,
-    data::DataConn,
+    data::{model::NETDOX_PLUGIN, DataConn},
     error::{NetdoxError, NetdoxResult},
     process_err,
 };
-
-const NETDOX_PLUGIN: &str = "netdox";
 
 pub async fn process(mut con: Box<dyn DataConn>) -> NetdoxResult<()> {
     let dns = con.get_dns().await?;

src/data/model.rs
@@ -11,6 +11,8 @@ use crate::{
     process_err, redis_err,
 };
 
+pub const NETDOX_PLUGIN: &str = "netdox";
+
 pub const DEFAULT_NETWORK_KEY: &str = "default_network";
 pub const CHANGELOG_KEY: &str = "changelog";
 pub const DNS_KEY: &str = "dns";

commit 17e1c6b1cfc65ed231f2cc3e0358fece86e629db

chore: Fixed imports broken by 99b7fee

src/process.rs
@@ -6,7 +6,10 @@ use std::collections::{hash_map::Entry, HashMap, HashSet};
 use paris::warn;
 
 use crate::{
-    data::{model::NETDOX_PLUGIN, DataConn},
+    data::{
+        model::{NetworkSuperSet, Node, RawNode, DNS, NETDOX_PLUGIN},
+        DataConn,
+    },
     error::{NetdoxError, NetdoxResult},
     process_err,
 };
@@ -17,7 +20,7 @@ pub async fn process(mut con: Box<dyn DataConn>) -> NetdoxResult<()> {
     for node in resolve_nodes(&dns, raw_nodes)? {
         con.put_node(&node).await?;
 
-        // TODO figure out a stable alg for this
+        // TODO stabilize this https://gitlab.allette.com.au/allette/netdox/netdox-redis/-/issues/47
         for dns_name in node.dns_names {
             con.put_dns_metadata(
                 &dns_name,