c30bdc932ee727d28780c9d4cf08961a913a6e27
Linus Kirkwood <linuskirkwood@gmail.com>
2025-01-13 18:07:00 +1100

    fix: Improved location assignment logic

--- src/config/remote.rs
+++ src/config/remote.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{hash_map::Entry, HashMap, HashSet},
+    collections::{HashMap, HashSet},
     net::Ipv4Addr,
 };
 
@@ -28,54 +28,93 @@ pub struct RemoteConfig {
 impl RemoteConfig {
     /// Sets the location metadata key on all applicable objects in the datastore.
     pub async fn set_locations(&self, mut con: DataStore) -> NetdoxResult<()> {
-        let mut matches = HashMap::new();
-        for name in con.get_dns_names().await? {
+        let dns = con.get_dns().await?;
+        for name in &dns.qnames {
             if let Some((_, uq_name)) = name.rsplit_once(']') {
                 if let Ok(ipv4) = uq_name.parse::<Ipv4Addr>() {
-                    for subnet in self.locations.keys() {
-                        if subnet.contains(&ipv4) {
-                            match matches.entry(name.clone()) {
-                                Entry::Vacant(entry) => {
-                                    entry.insert(*subnet);
-                                }
-                                Entry::Occupied(mut entry) => {
-                                    if subnet.prefix_len() < entry.get().prefix_len() {
-                                        entry.insert(*subnet);
-                                    }
+                    if let Some(subnet) = self.locate_ipv4(&ipv4) {
+                        self.set_dns_location(&mut con, name, subnet).await?;
+                    }
+                } else {
+                    let subnet = dns
+                        .forward_march(name)
+                        .iter()
+                        .filter_map(|term| {
+                            if let Some((_, uq_term)) = term.rsplit_once(']') {
+                                if let Ok(ipv4) = uq_term.parse::<Ipv4Addr>() {
+                                    return self.locate_ipv4(&ipv4);
                                 }
                             }
+                            None
+                        })
+                        .min_by(|subn_a, subn_b| subn_a.prefix_len().cmp(&subn_b.prefix_len()));
 
-                            if let Some(node_id) = con.get_dns_metadata(&name).await?.get("_node") {
-                                let node = con.get_node(node_id).await?;
-                                con.put_node_metadata(
-                                    &node,
-                                    LOCATIONS_PLUGIN,
-                                    HashMap::from([(
-                                        LOCATIONS_META_KEY,
-                                        self.locations.get(subnet).unwrap().as_ref(),
-                                    )]),
-                                )
-                                .await?;
-                            }
-                        }
+                    if let Some(subnet) = subnet {
+                        self.set_dns_location(&mut con, name, subnet).await?;
                     }
                 }
             }
         }
 
-        for (ipv4, subnet) in matches {
-            con.put_dns_metadata(
-                &ipv4,
-                LOCATIONS_PLUGIN,
-                HashMap::from([(
-                    LOCATIONS_META_KEY,
-                    self.locations.get(&subnet).unwrap().as_ref(),
-                )]),
-            )
-            .await?;
+        Ok(())
+    }
+
+    fn locate_ipv4(&self, ipv4: &Ipv4Addr) -> Option<&Ipv4Net> {
+        let mut best_subnet: Option<&Ipv4Net> = None;
+        for subnet in self.locations.keys() {
+            if subnet.contains(ipv4) {
+                if let Some(_subnet) = best_subnet {
+                    if subnet.prefix_len() < _subnet.prefix_len() {
+                        best_subnet = Some(subnet);
+                    }
+                } else {
+                    best_subnet = Some(subnet);
+                }
+            }
+        }
+
+        best_subnet
+    }
+
+    async fn set_dns_location(
+        &self,
+        con: &mut DataStore,
+        name: &str,
+        subnet: &Ipv4Net,
+    ) -> NetdoxResult<()> {
+        con.put_dns_metadata(
+            name,
+            LOCATIONS_PLUGIN,
+            HashMap::from([(
+                LOCATIONS_META_KEY,
+                self.locations.get(&subnet).unwrap().as_ref(),
+            )]),
+        )
+        .await?;
+
+        if let Some(node_id) = con.get_dns_metadata(&name).await?.get("_node") {
+            self.set_node_location(con, node_id, subnet).await?;
         }
 
         Ok(())
+    }
+
+    async fn set_node_location(
+        &self,
+        con: &mut DataStore,
+        node_id: &str,
+        subnet: &Ipv4Net,
+    ) -> NetdoxResult<()> {
+        let node = con.get_node(node_id).await?;
+        con.put_node_metadata(
+            &node,
+            LOCATIONS_PLUGIN,
+            HashMap::from([(
+                LOCATIONS_META_KEY,
+                self.locations.get(&subnet).unwrap().as_ref(),
+            )]),
+        )
+        .await
     }
 
     /// Sets label-associated metadata to all applicable objects in the datastore.
